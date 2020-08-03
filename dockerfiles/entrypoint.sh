0ace494be3d0aef14bf41826e63638afd0db31ce
ryan craig <rcraig@melaleuca.com>
2020-08-03 15:07:47 -0600

    Change entrypoint.sh

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -6,7 +6,7 @@ if [ $2 = "show" ]
 then
   cd ${GITHUB_WORKSPACE}/live/biz/consul-servers
   terraform init -input=false -backend-config="backend.hcl" -no-color
-  _outcome=$(terraform show -json -no-color ${TFPLAN} | jq 1>)
+  _outcome=$(terraform show -json -no-color ${TFPLAN} 2>&1)
 else
   exec "$@"
 fi
