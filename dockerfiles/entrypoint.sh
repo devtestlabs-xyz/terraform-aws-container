efbd577038a832b49a8c17f04baae1ac430dc57c
ryan craig <rcraig@melaleuca.com>
2020-08-04 03:09:27 -0600

    Change entrypoint.sh

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -12,19 +12,19 @@ cd ${GITHUB_WORKSPACE}/${_TF_CONFIG_PATH}
 if [ "$1" = "show" ]; then 
   terraform init -input=false -backend-config="backend.hcl" -no-color
 #   terraform show -json -no-color ${TFPLAN}
-  _outcome=$(terraform "$@")
+  _outcome=$(terraform $1 $2)
 elif [ "$1" = "init" ]; then
-  _outcome=$(terraform "$@")
+  _outcome=$(terraform $1 $2)
 elif [ "$1" = "fmt" ]; then 
-  _outcome=$(terraform "$@")
+  _outcome=$(terraform $1 $2)
 elif [ "$1" = "validate" ]; then 
-  _outcome=$(terraform "$@")
+  _outcome=$(terraform $1 $2)
 elif [ "$1" = "plan" ]; then 
-  _outcome=$(terraform "$@")
+  _outcome=$(terraform $1 $2)
 elif [ "$1" = "apply" ]; then
-  _outcome=$(terraform "$@")
+  _outcome=$(terraform $1 $2)
 elif [ "$1" = "destroy" ]; then 
-  _outcome=$(terraform "$@")
+  _outcome=$(terraform $1 $2)
 else
   echo "Command is not currently supported."
 fi
