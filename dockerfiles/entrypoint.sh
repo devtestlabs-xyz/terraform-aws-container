5d29a1567952fe2553c2aa398adc27af7f15c535
ryan craig <rcraig@melaleuca.com>
2020-08-04 13:45:22 -0600

    Change entrypoint.sh

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -7,21 +7,28 @@ cd ${GITHUB_WORKSPACE}/${_TF_WORKING_DIR}
 
 if [ "$1" = "show" ]; then 
   terraform init -input=false -backend-config="backend.hcl" -no-color
-  _outcome=$(terraform $1 $2)
+  _outcome=$(terraform $1 $2); _exitcode=$?
+
 elif [ "$1" = "init" ]; then
-  _outcome=$(terraform $1 $2)
+  _outcome=$(terraform $1 $2); _exitcode=$?
+
 elif [ "$1" = "fmt" ]; then 
-  _outcome=$(terraform $1 $2)
+  _outcome=$(terraform $1 $2); _exitcode=$?
+
 elif [ "$1" = "validate" ]; then 
-  _outcome=$(terraform $1 $2)
+  _outcome=$(terraform $1 $2); _exitcode=$?
+
 elif [ "$1" = "plan" ]; then 
-  _outcome=$(terraform $1 $2)
+  _outcome=$(terraform $1 $2); _exitcode=$?
+
 elif [ "$1" = "apply" ]; then
-  _outcome=$(terraform $1 $2)
+  _outcome=$(terraform $1 $2); _exitcode=$?
+
 # elif [ "$1" = "destroy" ]; then 
 #   _outcome=$(terraform $1 $2)
+
 else
-  echo "Command is not currently supported."
+  echo "Command is not currently supported."; _exitcode=3
 fi
 
 # Fix for multi-line content
@@ -31,3 +38,4 @@ _outcome="${_outcome//'%'/'%25'}"
 _outcome="${_outcome//$'\n'/'%0A'}"
 _outcome="${_outcome//$'\r'/'%0D'}"
 echo "::set-output name=outcome::$_outcome"
+echo "::set-output name=exitcode::$_exitcode"
