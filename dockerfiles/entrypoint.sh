cd6e305f57ea9852ee293747b93b8d0146f6c5de
ryan craig <rcraig@melaleuca.com>
2020-08-04 11:33:36 -0600

    Update entrypoint for Github actions

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -2,16 +2,11 @@
 
 set -e
 
-#STDOUT_FILE="_stdout"
-#touch $STDOUT_FILE
-
-cd ${GITHUB_WORKSPACE}/${_TF_CONFIG_PATH}
-
-#exec &> >(tee -a "$STDOUT_FILE")
+# Change path to Terraform working directory
+cd ${GITHUB_WORKSPACE}/${_TF_WORKING_DIR}
 
 if [ "$1" = "show" ]; then 
   terraform init -input=false -backend-config="backend.hcl" -no-color
-#   terraform show -json -no-color ${TFPLAN}
   _outcome=$(terraform $1 $2)
 elif [ "$1" = "init" ]; then
   _outcome=$(terraform $1 $2)
@@ -23,8 +18,8 @@ elif [ "$1" = "plan" ]; then
   _outcome=$(terraform $1 $2)
 elif [ "$1" = "apply" ]; then
   _outcome=$(terraform $1 $2)
-elif [ "$1" = "destroy" ]; then 
-  _outcome=$(terraform $1 $2)
+# elif [ "$1" = "destroy" ]; then 
+#   _outcome=$(terraform $1 $2)
 else
   echo "Command is not currently supported."
 fi
